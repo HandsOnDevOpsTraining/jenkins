d86e0e28573eb7868d2b69938cb46c40290c65e7
Madan Lanka <admin@lankascloudworks.com>
2023-04-27 23:43:37 +1000

    Docker CI Pipeline code added

---
+++ docker-ci-pipeline/docker-ci-pipeline.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+    
+    environment {
+        DOCKER_REGISTRY = "hub.docker.com"
+        DOCKER_REPO = "hello-ui"
+        BUILD_NUMBER = "1.0.0"
+        DOCKER_IMAGE = "handsondevops/${DOCKER_REPO}"
+        DOCKER_USERNAME = "handsondevops"
+        DOCKER_PASSWORD = credentials('dockerhub-password')
+    }
+    
+    stages {
+        stage('Clone repository') {
+            steps {
+                git branch: 'main', url: 'https://github.com/HandsOnDevOpsTraining/docker-sample-ui-app.git'
+            }
+        }
+        
+        stage('Build Docker image') {
+            steps {
+                sh 'docker build -t ${DOCKER_IMAGE}:${BUILD_NUMBER} .'
+            }
+        }
+        
+        stage('Push Docker Image') {
+            steps {
+                withCredentials([string(credentialsId: 'dockerhub-password', variable: 'dockerhub-password')]) {
+                    sh "docker login -u handsondevops -p ${DOCKER_PASSWORD}"
+                    sh "docker push ${DOCKER_IMAGE}:${BUILD_NUMBER}"
+                }
+            }
+        }
+    }
+}
---
+++ docker-ci-pipeline/docker-ci-pipeline.md
@@ -0,0 +1,23 @@
+# Integrate SonarQube with Jenkins 
+
+## 🧰 Prerequisites
+1. A Jenkins server 
+
+
+## Integration Steps 
+
+### On Jenkins server 
+
+1. Configure hub.docker.com credentials 
+   - Navigate to Jenkins-> Manage Jenkins -> Credentials -> System & Global (Domain) -> Add credentials -> Add a secret variable with name "dockerhub-password" & value as Docker Hub Password
+2. Run Pipeline job 
+   - Use help of chatgpt to create a pipeline script
+   - E.g. create a Jenkins pipeline script to clone the code from github public repo and main branch, publish docker image sample-ui:1.0.0 to dockerhub using username handsondevops and with credentials from encrypted password 
+
+## 🧹 CleanUp  
+
+   Stop Jenkins service and delete the instance 
+
+   
+## 🔗 My Profile
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/madan-lanka-0368a9b)
