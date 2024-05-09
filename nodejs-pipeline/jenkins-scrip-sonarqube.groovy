904883b4876f0698107ac88538cc1844e35d7b92
lankamadangit <lankamadan@gmail.com>
2024-05-09 20:15:59 +1000

    Updated the urls

--- nodejs-pipeline/jenkins-scrip-sonarqube.groovy
+++ nodejs-pipeline/jenkins-scrip-sonarqube.groovy
@@ -1,10 +1,15 @@
 pipeline {
     agent any
 
+    environment {
+        // GITHUB_TOKEN = credentials('github-token')
+        SONAR_TOKEN = credentials('nodejs-secret')
+    }
+
     stages {
         stage('Clone repository') {
             steps {
-                git branch: 'main', url: 'https://github.com/HandsOnDevOpsTraining/reactjs-ui-demo.git'
+                git branch: 'main', url: 'https://github.com/HandsOnDevOpsTraining/SonarQubeNodeJS.git'
             }
         }
 
